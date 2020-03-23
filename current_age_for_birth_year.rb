d8bc5d908d3f2390c33df708fb9b879de479a5c8
Calvin McDowell <calvinmac40@hotmail.com>
2020-03-23 14:52:55 +0000

    Automatically backed up by Learn

--- current_age_for_birth_year.rb
+++ current_age_for_birth_year.rb
@@ -1,10 +1,5 @@
 require 'pry' 
 
 def current_age_for_birth_year(year)
-  age = 1 
-  birth_year = "#{year}"
-  current_age = "#{age += 1}"
-  birthday = []
-  while age > 0 
-  
+  age_of_person = year
 end
